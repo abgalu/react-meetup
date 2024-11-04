f21fb63c41efddb430a4aa65bc79422813cc68e1
abraham.garcia.lujan.contractor <abraham.garcia.lujan.contractor@bbva.com>
2024-11-04 09:11:08 +0100

    test(MeetupItem): add test to add and remove favorites

--- src/components/meetups/MeetupItem.test.js
+++ src/components/meetups/MeetupItem.test.js
@@ -1,8 +1,59 @@
-/* eslint-disable testing-library/no-debugging-utils */
+import { fireEvent, render, screen } from "@testing-library/react";
 import { shallow } from "enzyme";
 import MeetupItem from "./MeetupItem";
 
+const item = {
+  id: "1",
+  title: "Sample Meetup",
+  address: "123 Meetup St.",
+  description: "A sample meetup for testing.",
+  image: "sample.jpg",
+};
+const addFavorite = jest.fn();
+const removeFavorite = jest.fn();
+
 test("<MeetupItem/> renders without crashing", () => {
-  const wrapper = shallow(<MeetupItem />);
+  const wrapper = shallow(
+    <MeetupItem
+      item={item}
+      addFavorite={addFavorite}
+      removeFavorite={removeFavorite}
+      isFavorite={false}
+    />
+  );
   expect(wrapper.exists()).toBe(true);
+});
+
+test("should add to favorites", () => {
+  render(
+    <MeetupItem
+      item={item}
+      addFavorite={addFavorite}
+      removeFavorite={removeFavorite}
+      isFavorite={false}
+    />
+  );
+
+  const button = screen.getByRole("button", { name: /add to favorites/i });
+  fireEvent.click(button);
+
+  expect(addFavorite).toHaveBeenCalledWith(item);
+});
+
+test("should remove from favorites", () => {
+  render(
+    <MeetupItem
+      item={item}
+      addFavorite={addFavorite}
+      removeFavorite={removeFavorite}
+      isFavorite
+    />
+  );
+
+  const button = screen.getByRole("button", {
+    name: /remove from favorites/i,
+  });
+  fireEvent.click(button);
+
+  expect(removeFavorite).toHaveBeenCalledWith(item.id);
 });
